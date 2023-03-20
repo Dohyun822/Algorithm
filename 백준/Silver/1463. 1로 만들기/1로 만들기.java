a506661cc84ce401042dfabc85f9d344a1d24a12
Dohyun822 <98006584+Dohyun822@users.noreply.github.com>
2023-03-20 23:51:48 +0900

    [Silver III] Title: 1로 만들기, Time: 104 ms, Memory: 15528 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static int N;
+	private static int[] dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+		dp = new int[1000001];
+
+		for (int i = 2; i <= N; i++) {
+			dp[i] = dp[i - 1] + 1;
+
+			if (i % 3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+			}
+			if (i % 2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+			}
+		}
+		System.out.println(dp[N]);
+
+	}// end of main
+}// end of class
