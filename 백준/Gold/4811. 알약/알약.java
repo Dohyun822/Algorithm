1c78b8374c723ee5380fa043e547a1fc48fc0829
Dohyun822 <98006584+Dohyun822@users.noreply.github.com>
2023-03-30 23:47:58 +0900

    [Gold V] Title: 알약, Time: 92 ms, Memory: 13264 KB -BaekjoonHub

---
+++ 백준/Gold/4811. 알약/알약.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	private static int N;
+	private static long[] dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		dp = new long[31];
+		dp[0] = 1;
+		dp[1] = 1;
+		dp[2] = 2;
+
+		while (true) {
+			N = Integer.parseInt(br.readLine());
+			if (N == 0) {
+				break;
+			} else {
+				for (int i = 3; i <= 30; i++) {
+					dp[i] = 0;
+					for (int j = 0; j < i; j++) {
+						dp[i] += dp[j] * dp[i - 1 - j];
+					}
+				}
+			}
+			sb.append(dp[N]).append("\n");
+
+		}
+
+		System.out.println(sb.toString());
+	}
+}
