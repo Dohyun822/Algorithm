ded0cac7b89bf954d2810969ca637aa27bb7d340
Dohyun822 <98006584+Dohyun822@users.noreply.github.com>
2023-04-01 00:29:37 +0900

    [Gold V] Title: 알약, Time: 76 ms, Memory: 11384 KB -BaekjoonHub

--- 백준/Gold/4811. 알약/알약.java
+++ 백준/Gold/4811. 알약/알약.java
@@ -15,19 +15,20 @@ public class Main {
 		dp[1] = 1;
 		dp[2] = 2;
 
+		for (int i = 3; i <= 30; i++) {
+			dp[i] = 0;
+			for (int j = 0; j < i; j++) {
+				dp[i] += dp[j] * dp[i - 1 - j];
+			}
+		}
+
 		while (true) {
 			N = Integer.parseInt(br.readLine());
 			if (N == 0) {
 				break;
 			} else {
-				for (int i = 3; i <= 30; i++) {
-					dp[i] = 0;
-					for (int j = 0; j < i; j++) {
-						dp[i] += dp[j] * dp[i - 1 - j];
-					}
-				}
+				sb.append(dp[N]).append("\n");
 			}
-			sb.append(dp[N]).append("\n");
 
 		}
 
