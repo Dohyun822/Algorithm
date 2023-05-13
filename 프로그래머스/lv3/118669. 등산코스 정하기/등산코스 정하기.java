8586b9b471ce4d514701f29e5a17205c05c66440
Dohyun822 <98006584+Dohyun822@users.noreply.github.com>
2023-05-13 17:37:35 +0900

    [level 3] Title: 등산코스 정하기, Time: 650.74 ms, Memory: 159 MB -BaekjoonHub

--- 프로그래머스/lv3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/lv3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -1,44 +1,89 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class Solution {
-    public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        int[][] dp = new int[n+1][m+1];
+	private static class Node {
+		int v, weight; // 노드, 가중치
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                dp[i][j] = Math.abs(r - i) + Math.abs(c - j);
-            }
-        }
+		Node(int v, int weight) {
+			this.v = v;
+			this.weight = weight;
+		}
+	}
 
-        if (dp[x][y] > k || dp[x][y] % 2 != k % 2) {
-            return "impossible";
-        }
+	static List<List<Node>> graph;
+	static boolean[] visit;
+	static int[] dist;
 
-        int nr = x, nc = y, nk = k;
-        StringBuilder answer = new StringBuilder();
+	public static int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
+		graph = new ArrayList<>();
+		dist = new int[n + 1];
+		visit = new boolean[n + 1];
+		Arrays.fill(dist, Integer.MAX_VALUE);
 
-        while (nk > 0) {
-            int[] result = go(nr, nc, nk, dp, n, m);
-            nr = result[0];
-            nc = result[1];
-            nk = result[2];
-            answer.append((char)result[3]);
-        }
+		for (int i = 0; i < n + 1; i++) {
+			graph.add(new ArrayList<>());
+		}
+		for (int[] path : paths) {
+			int start = path[0];
+			int end = path[1];
+			int weight = path[2];
 
-        return answer.toString();
-    }
+			graph.get(start).add(new Node(end, weight));
+			graph.get(end).add(new Node(start, weight));
 
-    private int[] go(int nr, int nc, int nk, int[][] dp, int n, int m) {
-        if (nr+1 <= n && dp[nr+1][nc] <= nk-1) {
-            return new int[] {nr+1, nc, nk-1, 'd'};
-        }
-        if (nc-1 >= 1 && dp[nr][nc-1] <= nk-1) {
-            return new int[] {nr, nc-1, nk-1, 'l'};
-        }
-        if (nc+1 <= m && dp[nr][nc+1] <= nk-1) {
-            return new int[] {nr, nc+1, nk-1, 'r'};
-        }
-        if (nr-1 >= 1 && dp[nr-1][nc] <= nk-1) {
-            return new int[] {nr-1, nc, nk-1, 'u'};
-        }
-        return null;
-    }
+		}
+
+		dijkstra(gates, summits);
+
+		int mn = Integer.MAX_VALUE; // 산봉우리 번호
+		int mw = Integer.MAX_VALUE; // 최솟값
+
+		Arrays.sort(summits);
+
+		for (int summit : summits) {
+			if (dist[summit] < mw) {
+				mn = summit;
+				mw = dist[summit];
+			}
+		}
+		return new int[] { mn, mw };
+	}
+
+	static void dijkstra(int[] gates, int[] summits) {
+		PriorityQueue<Node> q = new PriorityQueue<>((o1, o2) -> o1.weight - o2.weight);
+		for (int gate : gates) {
+			q.add(new Node(gate, 0));
+			dist[gate] = 0;
+		}
+
+		List<Integer> summitList = new ArrayList<>();
+
+		for (int summit : summits) {
+			summitList.add(summit);
+		}
+
+		while (!q.isEmpty()) {
+			Node now = q.poll();
+
+			if (now.weight > dist[now.v]) {
+				continue;
+			}
+
+			if (summitList.contains(now.v)) {
+				continue;
+			}
+
+			for (Node next : graph.get(now.v)) {
+				int dis = Math.max(dist[now.v], next.weight);
+				if (dist[next.v] > dis) {
+					dist[next.v] = dis;
+					q.add(new Node(next.v, dis));
+				}
+			}
+		}
+	}
+
 }
