ed329b7051adc5fd31981b56dbb755890f7842f0
Dohyun822 <98006584+Dohyun822@users.noreply.github.com>
2023-06-20 17:16:45 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 572 ms, Memory: 57076 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,40 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+
+	private static int N;
+	private static int M;
+	private static int[] prefix_sum;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
 
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-		st = new StringTokenizer(br.readLine());
+		prefix_sum = new int[N + 1];
 
-		int[] sum = new int[N];
-		sum[0] = Integer.parseInt(st.nextToken());
-		for (int i = 1; i < sum.length; i++) {
-			sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 1; i <= N; i++) {
+			prefix_sum[i] += Integer.parseInt(st.nextToken()) + prefix_sum[i - 1];
 		}
 
-		for (int k = 0; k < M; k++) {
-			int answer = 0;
-			st = new StringTokenizer(br.readLine());
-			int i = Integer.parseInt(st.nextToken()) - 1;
-			int j = Integer.parseInt(st.nextToken()) - 1;
-			if (i == 0) {
-				answer = sum[j];
-			} else {
-				answer = sum[j] - sum[i - 1];
-			}
-			sb.append(answer+"\n");
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+
+			sb.append(prefix_sum[end] - prefix_sum[start - 1]).append("\n");
 		}
+
 		System.out.println(sb.toString());
 
-	}// end of main
-}// end of class
+	}
+}
