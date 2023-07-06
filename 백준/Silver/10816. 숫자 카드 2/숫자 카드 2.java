cbe73f8aff29f6400c6a728188ee6610dc1d536a
Dohyun822 <98006584+Dohyun822@users.noreply.github.com>
2023-07-06 15:52:23 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1184 ms, Memory: 185076 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,65 +1,79 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.Stream;
-import java.math.*;
 
 public class Main {
-	public static int lowerBound(int[] arr, int key) {
-		int lo = 0;
-		int hi = arr.length;
-		
-		while(lo<hi) {
-			int mid = (lo+hi)/2;
-			
-			if(key <= arr[mid]) {
-				hi = mid;
-			}
-			else {
-				lo = mid + 1;
-			}
-		}
-		return lo;
-	}
-	public static int upperBound(int[] arr, int key) {
-		int lo = 0;
-		int hi = arr.length;
-		
-		while(lo<hi) {
-			int mid = (lo+hi)/2;
-			
-			if(key < arr[mid]) {
-				hi = mid;
-			}
-			else {
-				lo = mid + 1;
-			}
-		}
-		return lo;
-	}
-	public static void main(String[] args) throws IOException {
+	private static int N;
+	private static int[] arr;
+	private static int M;
+	private static int[] arr2;
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int [N];
-		st = new StringTokenizer(br.readLine());
-		
-		for(int i = 0; i<N; i++) {
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		Arrays.sort(arr);
-		
-		int M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		
-		for(int i = 0; i<M; i++) {
-			int key = Integer.parseInt(st.nextToken());
-			sb.append(upperBound(arr,key)-lowerBound(arr,key)).append(' ');
-		}
-		System.out.println(sb);
-		
 
+		M = Integer.parseInt(br.readLine());
+		arr2 = new int[M];
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < M; i++) {
+			arr2[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		for (int i = 0; i < M; i++) {
+			sb.append(findHighIndex(i) - findLowIndex(i)).append(" ");
+		}
+		System.out.println(sb.toString());
 	}
+
+	private static int findHighIndex(int i) {
+		int start = 0;
+		int end = N - 1;
+		int cnt = 0;
+		boolean chk = false;
+		while (start <= end) {
+			int mid = start + ((end - start) >> 1);
+			if (arr2[i] == arr[mid]) {
+				start = mid + 1;
+				cnt = mid;
+				chk = true;
+			} else if (arr2[i] > arr[mid]) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
+		if (chk) {
+			cnt++;
+		}
+		return cnt;
+	}
+
+	private static int findLowIndex(int i) {
+		int start = 0;
+		int end = N - 1;
+		int cnt = 0;
+
+		while (start <= end) {
+			int mid = start + ((end - start) >> 1);
+			if (arr2[i] == arr[mid]) {
+				end = mid - 1;
+				cnt = mid;
+			} else if (arr2[i] > arr[mid]) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
+		return cnt;
+	}
+
 }
