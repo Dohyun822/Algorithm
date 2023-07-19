0c7dbb117fd322cecf3bf9ddb8f7d6cee3dcd549
Dohyun822 <98006584+Dohyun822@users.noreply.github.com>
2023-07-20 01:59:21 +0900

    [Silver V] Title: 너의 평점은, Time: 88 ms, Memory: 11700 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<String, Double> grades = new HashMap<>();
+        grades.put("A+", 4.5);
+        grades.put("A0", 4.0);
+        grades.put("B+", 3.5);
+        grades.put("B0", 3.0);
+        grades.put("C+", 2.5);
+        grades.put("C0", 2.0);
+        grades.put("D+", 1.5);
+        grades.put("D0", 1.0);
+        grades.put("F", 0.0);
+
+        double totalCredits = 0.0;
+        double totalGradePoints = 0.0;
+        
+        for(int i = 0; i < 20; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            st.nextToken(); // Skip subject name
+            double credit = Double.parseDouble(st.nextToken());
+            String grade = st.nextToken();
+
+            if(!grade.equals("P")) {
+                totalCredits += credit;
+                totalGradePoints += credit * grades.get(grade);
+            }
+        }
+        
+        double GPA = totalGradePoints / totalCredits;
+        System.out.println(GPA);
+    }
+}
