140ccc5ae8036bf2e94ea970c2e08398afd07361
Dohyun822 <yup8229@gmail.com>
2023-11-24 12:57:37 +0900

    [Silver III] Title: N과 M (3), Time: 376 ms, Memory: 112012 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,45 +1,45 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
 	private static int N;
 	private static int M;
+	private static int[] arr;
 	private static int[] selected;
 	private static StringBuilder sb;
-	private static boolean[] visited;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
 		sb = new StringBuilder();
-
-		st = new StringTokenizer(br.readLine(), " ");
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
 		selected = new int[M];
-		visited = new boolean[N + 1];
 
-		recur(0, 1);
+		solve(0, 0);
 
 		System.out.println(sb.toString());
 
 	}
 
-	private static void recur(int cur, int start) {
-		if (cur == M) {
+	private static void solve(int cnt, int flag) {
+		if (cnt == M) {
 			for (int i : selected) {
 				sb.append(i).append(" ");
 			}
-			sb.append("\n");
+			sb.append('\n');
 			return;
 		}
-
 		for (int i = 1; i <= N; i++) {
-			selected[cur] = i;
-			recur(cur + 1, i + 1);
+//			if ((flag & 1 << i) != 0)
+//				continue;
+			selected[cnt] = i;
+			solve(cnt + 1, flag | 1 << i);
 		}
 	}
 }
