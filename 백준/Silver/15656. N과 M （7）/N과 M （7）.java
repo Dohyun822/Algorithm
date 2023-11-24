07c38ec2a28aed6030d631d10fba2927e7c98a64
Dohyun822 <yup8229@gmail.com>
2023-11-24 13:41:39 +0900

    [Silver III] Title: N과 M (7), Time: 612 ms, Memory: 220344 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.java
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -1,58 +1,56 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
 	private static int N;
 	private static int M;
+	private static int[] arr;
 	private static int[] selected;
 	private static StringBuilder sb;
-	private static boolean[] visited;
-	private static int[] numbers;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
 		sb = new StringBuilder();
-
-		st = new StringTokenizer(br.readLine(), " ");
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
-		numbers = new int[N];
+		arr = new int[N];
 		selected = new int[M];
-		visited = new boolean[N + 1];
 
 		st = new StringTokenizer(br.readLine(), " ");
+
 		for (int i = 0; i < N; i++) {
-			numbers[i] = Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		Arrays.sort(numbers);
-		recur(0, 0);
+		Arrays.sort(arr);
+
+		solve(0, 0, 0);
 
 		System.out.println(sb.toString());
 
 	}
 
-	private static void recur(int cur, int start) {
-		if (cur == M) {
+	private static void solve(int start, int cnt, int flag) {
+		if (cnt == M) {
 			for (int i : selected) {
 				sb.append(i).append(" ");
 			}
-			sb.append("\n");
+			sb.append('\n');
 			return;
 		}
-
-		for (int i = 0; i < N; i++) {
-//			if (visited[i])
+		for (int i = start; i < N; i++) {
+//			if ((flag & 1 << i) != 0)
 //				continue;
-			selected[cur] = numbers[i];
-//			visited[i] = true;
-			recur(cur + 1, i);
-//			visited[i] = false;
+			selected[cnt] = arr[i];
+
+			solve(start, cnt + 1, flag | 1 << i);
 		}
 	}
 }
