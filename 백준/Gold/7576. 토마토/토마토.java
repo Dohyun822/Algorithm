8fff618a8b689d591fcf95277dd8afd5710e1ca3
Dohyun822 <yup8229@gmail.com>
2024-03-05 14:52:46 +0900

    [Gold V] Title: 토마토, Time: 584 ms, Memory: 97944 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,85 +1,92 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static class tomato {
-		int r;
-		int c;
-
-		public tomato(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
-
-	}
-
 	private static int N;
 	private static int M;
 	private static int[][] map;
-	private static int[][] day;
-	static Queue<tomato> q = new ArrayDeque<tomato>();
+	private static boolean[][] visited;
 
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
-	static int answer;
+	private static int[] dr = { 1, -1, 0, 0 };
+	private static int[] dc = { 0, 0, -1, 1 };
+	private static int answer;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		map = new int[M][N];
-		day = new int[M][N];
+		N = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < M; i++) {
-			Arrays.fill(day[i], -1);
-		}
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
+		map = new int[N][M];
+		visited = new boolean[N][M];
+
+		boolean check = true;
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if (map[i][j] == 1) {
-					q.offer(new tomato(i, j));
-					day[i][j] = 0;
-				}
-			}
-		}
-		answer = 0;
-		bfs();
-		for (int i = 0; i < M; i++) {
-			for (int j = 0; j < N; j++) {
 				if (map[i][j] == 0) {
-					System.out.println(-1);
-					return;
+					check = false;
 				}
-				answer = Math.max(answer, day[i][j]);
 			}
 		}
+
+		if (check) {
+			System.out.println(0);
+			return;
+		}
+
+		answer = 0;
+
+		solve();
+
 		System.out.println(answer);
-	}// end of main
 
-	private static void bfs() {
+	}
+
+	private static void solve() {
+		Queue<int[]> q = new ArrayDeque<int[]>();
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (map[i][j] == 1) {
+					visited[i][j] = true;
+					q.offer(new int[] { i, j });
+				}
+			}
+		}
+
 		while (!q.isEmpty()) {
-			tomato temp = q.poll();
-
-			for (int i = 0; i < 4; i++) {
-				int nr = temp.r + dr[i];
-				int nc = temp.c + dc[i];
-				if (nr >= 0 && nr < M && nc >= 0 && nc < N) {
-					if (day[nr][nc] == -1 && map[nr][nc] == 0) {
+			int size = q.size();
+			for (int i = 0; i < size; i++) {
+				int[] point = q.poll();
+				int r = point[0];
+				int c = point[1];
+				for (int j = 0; j < dr.length; j++) {
+					int nr = r + dr[j];
+					int nc = c + dc[j];
+					if (nr >= 0 && nr < N && nc >= 0 && nc < M && !visited[nr][nc] && map[nr][nc] == 0) {
 						map[nr][nc] = 1;
-						day[nr][nc] = day[temp.r][temp.c] + 1;
-						q.offer(new tomato(nr, nc));
+						visited[nr][nc] = true;
+						q.offer(new int[] { nr, nc });
 					}
 				}
-
+			}
+			if (!q.isEmpty()) {
+				answer++;
 			}
 		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (map[i][j] == 0) {
+					answer = -1;
+					return;
+				}
+			}
+		}
+
 	}
-}// end of class
+}
