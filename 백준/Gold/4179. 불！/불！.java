cc62c922b23ee7979cfc59de08e6db0cd9b71d36
Dohyun822 <yup8229@gmail.com>
2024-03-05 15:08:56 +0900

    [Gold IV] Title: 불!, Time: 464 ms, Memory: 53156 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -1,18 +1,16 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-	static int[] dr = { 1, -1, 0, 0 };
-	static int[] dc = { 0, 0, 1, -1 };
+public class Main {
+	private static int[] dr = { 1, -1, 0, 0 };
+	private static int[] dc = { 0, 0, -1, 1 };
 	private static int R;
 	private static int C;
 	private static char[][] map;
-	private static int[][] fireTime;
-	private static int[][] jihunTime;
-	private static Queue<int[]> fireQueue = new ArrayDeque<>();
-	private static Queue<int[]> jihunQueue = new ArrayDeque<>();
+	private static int[][] jihun;
+	private static int[][] fire;
 
-	public static void main(String args[]) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
@@ -20,79 +18,69 @@ class Main {
 		C = Integer.parseInt(st.nextToken());
 
 		map = new char[R][C];
-		fireTime = new int[R][C];
-		jihunTime = new int[R][C];
+		fire = new int[R][C];
+		jihun = new int[R][C];
 
 		for (int i = 0; i < R; i++) {
 			String s = br.readLine();
 			for (int j = 0; j < C; j++) {
 				map[i][j] = s.charAt(j);
-				fireTime[i][j] = -1;
-				jihunTime[i][j] = -1;
-				if (map[i][j] == 'F') {
-					fireQueue.offer(new int[] { i, j });
-					fireTime[i][j] = 0;
-				}
+			}
+		}
+
+		solve();
+
+	}
+
+	private static void solve() {
+		Queue<int[]> person = new ArrayDeque<int[]>();
+		Queue<int[]> bull = new ArrayDeque<int[]>();
+
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				jihun[i][j] = Integer.MAX_VALUE;
+				fire[i][j] = Integer.MAX_VALUE;
 				if (map[i][j] == 'J') {
-					jihunQueue.offer(new int[] { i, j });
-					jihunTime[i][j] = 0;
+					person.offer(new int[] { i, j });
+					jihun[i][j] = 0;
+				}
+				if (map[i][j] == 'F') {
+					bull.offer(new int[] { i, j });
+					fire[i][j] = 0;
 				}
 			}
 		}
 
-		fire();
-
-		int answer = jihun();
-
-		if (answer == -1) {
-			System.out.println("IMPOSSIBLE");
-		} else {
-			System.out.println(answer);
+		while (!bull.isEmpty()) {
+			int[] cur = bull.poll();
+			for (int i = 0; i < dr.length; i++) {
+				int nr = cur[0] + dr[i];
+				int nc = cur[1] + dc[i];
+				if (nr >= 0 && nr < R && nc >= 0 && nc < C && map[nr][nc] != '#' && fire[nr][nc] == Integer.MAX_VALUE) {
+					fire[nr][nc] = fire[cur[0]][cur[1]] + 1;
+					bull.offer(new int[] { nr, nc });
+				}
+			}
 		}
-	}
-
-	private static void fire() {
-		while (!fireQueue.isEmpty()) {
-			int[] cur = fireQueue.poll();
-			int r = cur[0];
-			int c = cur[1];
-
+		while (!person.isEmpty()) {
+			int[] cur = person.poll();
+			if (cur[0] == 0 || cur[1] == 0 || cur[0] == R - 1 || cur[1] == C - 1) {
+				System.out.println(jihun[cur[0]][cur[1]] + 1);
+				return;
+			}
 			for (int d = 0; d < 4; d++) {
-				int nr = r + dr[d];
-				int nc = c + dc[d];
-				if (nr >= 0 && nr < R && nc >= 0 && nc < C) {
-					if (map[nr][nc] == '.' && fireTime[nr][nc] == -1) {
-						fireTime[nr][nc] = fireTime[r][c] + 1;
-						fireQueue.offer(new int[] { nr, nc });
-					}
+				int nr = cur[0] + dr[d];
+				int nc = cur[1] + dc[d];
+				if (nr >= 0 && nr < R && nc >= 0 && nc < C && map[nr][nc] == '.' && jihun[nr][nc] == Integer.MAX_VALUE
+						&& (jihun[cur[0]][cur[1]] + 1 < fire[nr][nc])) {
+					jihun[nr][nc] = jihun[cur[0]][cur[1]] + 1;
+					person.offer(new int[] { nr, nc });
+
 				}
 			}
 		}
-	}
 
-	private static int jihun() {
-		while (!jihunQueue.isEmpty()) {
-			int[] cur = jihunQueue.poll();
-			int r = cur[0];
-			int c = cur[1];
+		System.out.println("IMPOSSIBLE");
 
-			if (r == 0 || c == 0 || r == R - 1 || c == C - 1) {
-				return jihunTime[r][c] + 1;
-			}
-
-			for (int d = 0; d < 4; d++) {
-				int nr = r + dr[d];
-				int nc = c + dc[d];
-				if (nr >= 0 && nr < R && nc >= 0 && nc < C) {
-					if (map[nr][nc] == '.' && jihunTime[nr][nc] == -1) {
-						if (fireTime[nr][nc] == -1 || jihunTime[r][c] + 1 < fireTime[nr][nc]) {
-							jihunTime[nr][nc] = jihunTime[r][c] + 1;
-							jihunQueue.offer(new int[] { nr, nc });
-						}
-					}
-				}
-			}
-		}
-		return -1;
 	}
 }
