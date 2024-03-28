2620451091377681ca2334163c441f4f39fa565e
Dohyun822 <yup8229@gmail.com>
2024-03-28 22:31:10 +0900

    [Gold V] Title: 무한 수열, Time: 92 ms, Memory: 11648 KB -BaekjoonHub

---
+++ 백준/Gold/1351. 무한 수열/무한 수열.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	private static long N;
+	private static long P;
+	private static long Q;
+	private static Map<Long, Long> memo = new HashMap<>();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		N = Long.parseLong(st.nextToken());
+		P = Long.parseLong(st.nextToken());
+		Q = Long.parseLong(st.nextToken());
+
+		System.out.println(findA(N));
+	}
+
+	private static long findA(long i) {
+		if (i == 0) {
+			return 1;
+		}
+
+		if (memo.containsKey(i)) {
+			return memo.get(i);
+		}
+
+		long result = findA(i / P) + findA(i / Q);
+
+		memo.put(i, result);
+
+		return result;
+	}
+}
