e6aadbf16f5ba4e2f335856c4570f5af787d21f8
Dohyun822 <yup8229@gmail.com>
2024-04-29 16:21:39 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 104 ms, Memory: 12136 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,37 +1,26 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	private static int a;
-	private static int b;
-	private static int c;
-	private static int d;
-	private static int e;
-	private static int f;
-	private static int X;
-	private static int Y;
-
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		a = Integer.parseInt(st.nextToken());
-		b = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		d = Integer.parseInt(st.nextToken());
-		e = Integer.parseInt(st.nextToken());
-		f = Integer.parseInt(st.nextToken());
 
-		ex: for (int i = -999; i <= 999; i++) {
-			for (int j = -999; j <= 999; j++) {
-				if (a * i + b * j == c && d * i + e * j == f) {
-					System.out.println(i + " " + j);
-					break ex;
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int D = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		int F = Integer.parseInt(st.nextToken());
+
+		for (int x = -999; x <= 999; x++) {
+			for (int y = -999; y <= 999; y++) {
+				if (A * x + B * y == C && D * x + E * y == F) {
+					System.out.println(x + " " + y);
+					return;
 				}
 			}
 		}
 
-	}// end of main
-}// end of class
+	}
+}
