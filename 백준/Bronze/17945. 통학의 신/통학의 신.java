766e9300b4866f6c5588b21c593404696e285013
Dohyun822 <yup8229@gmail.com>
2024-04-29 15:20:33 +0900

    [Bronze III] Title: 통학의 신, Time: 76 ms, Memory: 11476 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.java
+++ 백준/Bronze/17945. 통학의 신/통학의 신.java
@@ -1,37 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	private static int A;
-	private static int B;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
 
-		A = Integer.parseInt(st.nextToken());
-		B = Integer.parseInt(st.nextToken());
-
-		ArrayList<Integer> list = new ArrayList<Integer>();
-
-		list.add((int) (A * (-1) + Math.sqrt(A * A - B)));
-		list.add((int) (A * (-1) - Math.sqrt(A * A - B)));
-
-		if (list.get(0) == list.get(1)) {
-			System.out.println(list.get(0));
-		} else {
-			Collections.sort(list);
-			System.out.print(list.get(0) + " ");
-			System.out.println(list.get(1));
+		for (int i = -1000; i <= 1000; i++) {
+			if (i * i + 2 * A * i + B == 0) {
+				sb.append(i).append(" ");
+			}
 		}
 
+		System.out.println(sb.toString());
 	}
 }
