7d84e73d2e1ac944439c7e4bb8dbe0d990d44278
Dohyun822 <yup8229@gmail.com>
2024-04-29 16:42:29 +0900

    [Silver V] Title: 색종이, Time: 76 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[][] arr = new int[100][100];
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int answer = 0;
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+
+			for (int j = x; j < x + 10; j++) {
+				for (int k = y; k < y + 10; k++) {
+					if (arr[j][k] == 0) {
+						arr[j][k] = 1;
+						answer++;
+					}
+				}
+			}
+		}
+		System.out.print(answer);
+	}// end of main
+}// end of class
