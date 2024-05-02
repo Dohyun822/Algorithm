872b3b10b77a8661490008341eba5b495cabd73c
Dohyun822 <yup8229@gmail.com>
2024-05-02 17:08:09 +0900

    [Silver III] Title: N과 M (5), Time: 204 ms, Memory: 25464 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,58 +1,59 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	private static int[] selected;
 	private static int N;
 	private static int M;
-	private static int[] selected;
 	private static StringBuilder sb;
 	private static boolean[] visited;
-	private static int[] numbers;
+	private static int[] arr;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		sb = new StringBuilder();
-
-		st = new StringTokenizer(br.readLine(), " ");
 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
-		numbers = new int[N];
-		selected = new int[M];
-		visited = new boolean[N + 1];
-
 		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
-			numbers[i] = Integer.parseInt(st.nextToken());
-		}
 
-		Arrays.sort(numbers);
-		recur(0, 1);
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		visited = new boolean[N];
+		selected = new int[M];
+
+		recur(0, 0);
 
 		System.out.println(sb.toString());
 
 	}
 
-	private static void recur(int cur, int start) {
-		if (cur == M) {
-			for (int i : selected) {
-				sb.append(i).append(" ");
-			}
-			sb.append("\n");
+	private static void recur(int start, int depth) {
+		if (depth == M) {
+			print();
 			return;
 		}
 
 		for (int i = 0; i < N; i++) {
-			if (visited[i])
-				continue;
-			selected[cur] = numbers[i];
-			visited[i] = true;
-			recur(cur + 1, i);
-			visited[i] = false;
+			if (!visited[i]) {
+				visited[i] = true;
+				selected[depth] = arr[i];
+				recur(i, depth + 1);
+				visited[i] = false;
+			}
 		}
+	}
+
+	private static void print() {
+		for (int i = 0; i < M; i++) {
+			sb.append(selected[i]).append(" ");
+		}
+		sb.append('\n');
+
 	}
 }
