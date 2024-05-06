134650cc704e26933a311778a1385989af1458c5
Dohyun822 <yup8229@gmail.com>
2024-05-06 21:22:20 +0900

    [Silver II] Title: 차이를 최대로, Time: 104 ms, Memory: 12468 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
@@ -1,59 +1,55 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	private static int N;
 	private static int[] arr;
-	private static int answer;
+	private static int[] selected;
+	private static int max;
 	private static boolean[] visited;
-	private static int[] permuteArr;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		StringBuilder sb = new StringBuilder();
 
 		N = Integer.parseInt(br.readLine());
 
-		st = new StringTokenizer(br.readLine(), " ");
-
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		arr = new int[N];
+
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		answer = Integer.MIN_VALUE;
+		selected = new int[N];
 		visited = new boolean[N];
-		permuteArr = new int[N];
+		max = Integer.MIN_VALUE;
 
-		permute(0);
+		recur(0);
 
-		System.out.println(answer);
-
+		System.out.println(max);
 	}
 
-	private static void permute(int cur) {
-		if (cur == N) {
-			solve();
+	private static void recur(int depth) {
+		if (depth == N) {
+			check();
 			return;
 		}
-		for (int i = 0; i < arr.length; i++) {
+		for (int i = 0; i < N; i++) {
 			if (!visited[i]) {
+				selected[depth] = arr[i];
 				visited[i] = true;
-				permuteArr[cur] = arr[i];
-				permute(cur + 1);
+				recur(depth + 1);
 				visited[i] = false;
 			}
 		}
-
 	}
 
-	private static void solve() {
+	private static void check() {
 		int hap = 0;
 		for (int i = 0; i < N - 1; i++) {
-			hap += Math.abs(permuteArr[i] - permuteArr[i + 1]);
+			hap += Math.abs(selected[i] - selected[i + 1]);
 		}
-		answer = Math.max(answer, hap);
+		max = Math.max(max, hap);
 	}
+
 }
