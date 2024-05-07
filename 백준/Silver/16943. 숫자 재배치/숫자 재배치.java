7aed9c0215a27a45f942141a1ab983101055e2a9
Dohyun822 <yup8229@gmail.com>
2024-05-07 18:52:31 +0900

    [Silver I] Title: 숫자 재배치, Time: 416 ms, Memory: 101532 KB -BaekjoonHub

--- 백준/Silver/16943. 숫자 재배치/숫자 재배치.java
+++ 백준/Silver/16943. 숫자 재배치/숫자 재배치.java
@@ -2,45 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static String answer = "";
-    private static int B;
+	private static String answer;
+	private static int B;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        String A = st.nextToken();
-        B = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		String A = st.nextToken();
+		B = Integer.parseInt(st.nextToken());
 
-        char[] chars = A.toCharArray();
-        boolean[] visited = new boolean[A.length()];
-        
-        permutation("", chars, visited);
+		char[] chars = A.toCharArray();
+		boolean[] visited = new boolean[A.length()];
 
-        if (answer.isEmpty()) {
-            System.out.println(-1);
-        } else {
-            System.out.println(answer);
-        }
-    }
+		answer = "";
 
-    private static void permutation(String current, char[] chars, boolean[] visited) {
-        if (current.length() == chars.length) {
-            int num = Integer.parseInt(current);
-            if (num < B) {
-                if (answer.isEmpty() || Integer.parseInt(answer) < num) {
-                    answer = current;
-                }
-            }
-            return;
-        }
+		recur("", chars, visited);
 
-        for (int i = 0; i < chars.length; i++) {
-            if (!visited[i]) {
-                if (chars[i] == '0' && current.isEmpty()) continue;
-                visited[i] = true;
-                permutation(current + chars[i], chars, visited);
-                visited[i] = false;
-            }
-        }
-    }
+		if (answer.isEmpty()) {
+			System.out.println(-1);
+		} else {
+			System.out.println(answer);
+		}
+	}
+
+	private static void recur(String current, char[] chars, boolean[] visited) {
+		if (current.length() == chars.length) {
+			int num = Integer.parseInt(current);
+			if (num < B) {
+				if (answer.isEmpty() || Integer.parseInt(answer) < num) {
+					answer = current;
+				}
+			}
+			return;
+		}
+
+		for (int i = 0; i < chars.length; i++) {
+			if (!visited[i]) {
+				if (chars[i] == '0' && current.isEmpty())
+					continue;
+				visited[i] = true;
+				recur(current + chars[i], chars, visited);
+				visited[i] = false;
+			}
+		}
+	}
 }
