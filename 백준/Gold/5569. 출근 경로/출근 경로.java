0eb6201a45d313ce73e0a8fe4bf0ecb4dde5900b
Dohyun822 <yup8229@gmail.com>
2024-05-28 02:28:36 +0900

    [Gold IV] Title: 출근 경로, Time: 84 ms, Memory: 12724 KB -BaekjoonHub

--- 백준/Gold/5569. 출근 경로/출근 경로.java
+++ 백준/Gold/5569. 출근 경로/출근 경로.java
@@ -5,7 +5,6 @@ public class Main {
 	private static final int MOD = 100000;
 	private static int w;
 	private static int h;
-	private static int answer = 0;
 	private static long[][][][] dp;
 
 	public static void main(String[] args) throws Exception {
@@ -17,47 +16,45 @@ public class Main {
 
 		dp = new long[w + 1][h + 1][2][2];
 
-		for (int i = 0; i <= w; i++) {
-			for (int j = 0; j <= h; j++) {
-				Arrays.fill(dp[i][j][0], -1);
-				Arrays.fill(dp[i][j][1], -1);
+		for (int i = 1; i <= w; i++) {
+			for (int j = 1; j <= h; j++) {
+				dp[i][j][0][0] = 0;
+				dp[i][j][0][1] = 0;
+				dp[i][j][1][0] = 0;
+				dp[i][j][1][1] = 0;
 			}
 		}
 
-		recur(1, 1, 0, 0);
-		recur(1, 1, 1, 0);
+		dp[1][1][0][0] = 1;
+		dp[1][1][1][0] = 1;
 
-		System.out.println((recur(1, 1, 0, 0) + recur(1, 1, 1, 0)) % MOD);
-	}
-
-	static long recur(int x, int y, int dir, int canChange) {
-		if (x > w || y > h)
-			return 0;
-		if (x == w && y == h) {
-			return 1;
-		}
-
-		if (dp[x][y][dir][canChange] != -1) {
-			return dp[x][y][dir][canChange];
-		}
-
-		long count = 0;
-
-		if (dir == 0) {
-			count = count + recur(x, y + 1, 0, 1) % MOD;
-		} else {
-			count = count + recur(x + 1, y, 1, 1) % MOD;
-		}
-
-		if (canChange == 1) {
-			if (dir == 0) {
-				count = (count + recur(x + 1, y, 1, 0)) % MOD;
-			} else {
-				count = (count + recur(x, y + 1, 0, 0)) % MOD;
+		for (int x = 1; x <= w; x++) {
+			for (int y = 1; y <= h; y++) {
+				for (int dir = 0; dir < 2; dir++) {
+					for (int canChange = 0; canChange < 2; canChange++) {
+						if (dp[x][y][dir][canChange] > 0) {
+							if (dir == 0) { // 수평 방향
+								if (y + 1 <= h) {
+									dp[x][y + 1][0][1] = (dp[x][y + 1][0][1] + dp[x][y][0][canChange]) % MOD;
+								}
+								if (canChange == 1 && x + 1 <= w) {
+									dp[x + 1][y][1][0] = (dp[x + 1][y][1][0] + dp[x][y][0][canChange]) % MOD;
+								}
+							} else { // 수직 방향
+								if (x + 1 <= w) {
+									dp[x + 1][y][1][1] = (dp[x + 1][y][1][1] + dp[x][y][1][canChange]) % MOD;
+								}
+								if (canChange == 1 && y + 1 <= h) {
+									dp[x][y + 1][0][0] = (dp[x][y + 1][0][0] + dp[x][y][1][canChange]) % MOD;
+								}
+							}
+						}
+					}
+				}
 			}
 		}
 
-		dp[x][y][dir][canChange] = count;
-		return dp[x][y][dir][canChange];
+		long result = (dp[w][h][0][0] + dp[w][h][0][1] + dp[w][h][1][0] + dp[w][h][1][1]) % MOD;
+		System.out.println(result);
 	}
 }
