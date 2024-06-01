f557980b12a0fb518bc28f738200f8b2710cb8f5
Dohyun822 <yup8229@gmail.com>
2024-06-01 20:28:57 +0900

    [Silver II] Title: 창고 다각형, Time: 196 ms, Memory: 18488 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -1,65 +1,69 @@
-import java.awt.List;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	static class Stick {
-		int idx;
-		int h;
+		int pos;
+		int height;
 
-		public Stick(int idx, int h) {
-			this.idx = idx;
-			this.h = h;
+		public Stick(int pos, int height) {
+			this.pos = pos;
+			this.height = height;
 		}
-
 	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		ArrayList<Stick> arr = new ArrayList<>();
+		Stick[] sticks = new Stick[N];
+
+		int maxPos = 0;
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			Stick stick = new Stick(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-			arr.add(stick);
-		}
-		Collections.sort(arr, (o1, o2) -> {
-			return o1.idx - o2.idx;
-		});
-
-		int Maxhigh = 0;
-		int Maxhigh_idx = 0;
-		for (int i = 0; i < arr.size(); i++) { // 만약 제일 높은 기둥이 여러개면 맨 나중 기둥 인덱스 저장
-			if (Maxhigh <= arr.get(i).h) {
-				Maxhigh = arr.get(i).h;
-				Maxhigh_idx = i;
-			}
-		}
-		int answer = Maxhigh;
-		// 왼쪽부터 가장 높은 기둥까지의 넓이
-		Stick now = arr.get(0);
-		for (int i = 1; i <= Maxhigh_idx; i++) {
-			Stick right = arr.get(i);
-			if (right.h >= now.h) {// 더 큰 기둥 만남
-				answer += now.h * (right.idx - now.idx);
-				now = right;
+			int pos = Integer.parseInt(st.nextToken());
+			int height = Integer.parseInt(st.nextToken());
+			sticks[i] = new Stick(pos, height);
+			if (pos > maxPos) {
+				maxPos = pos;
 			}
 		}
 
-		// 오른쪽부터 가장 높은 기둥까지의 넓이
-		now = arr.get(N - 1);
-		for (int i = N - 2; i >= Maxhigh_idx; i--) {
-			Stick left = arr.get(i);
-			if (left.h >= now.h) {
-				answer += now.h * (now.idx - left.idx);
-				now = left;
+		Arrays.sort(sticks, (a, b) -> a.pos - b.pos);
+
+		int[] heights = new int[maxPos + 1];
+		for (Stick stick : sticks) {
+			heights[stick.pos] = stick.height;
+		}
+
+		int[] leftMax = new int[maxPos + 1];
+		int[] rightMax = new int[maxPos + 1];
+
+		leftMax[0] = heights[0];
+		for (int i = 1; i <= maxPos; i++) {
+			leftMax[i] = Math.max(leftMax[i - 1], heights[i]);
+		}
+
+		rightMax[maxPos] = heights[maxPos];
+		for (int i = maxPos - 1; i >= 0; i--) {
+			rightMax[i] = Math.max(rightMax[i + 1], heights[i]);
+		}
+
+		int maxH = 0, maxIdx = 0;
+		for (int i = 0; i <= maxPos; i++) {
+			if (heights[i] > maxH) {
+				maxH = heights[i];
+				maxIdx = i;
 			}
 		}
-		System.out.println(answer);
 
-	}// end of main
-}// end of class
+		int area = 0;
+		for (int i = 0; i <= maxIdx; i++) {
+			area += leftMax[i];
+		}
+		for (int i = maxPos; i > maxIdx; i--) {
+			area += rightMax[i];
+		}
+
+		System.out.println(area);
+	}
+}
