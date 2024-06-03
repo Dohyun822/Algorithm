f5007a7c5c27409abd2345cd11eb738c64252619
Dohyun822 <yup8229@gmail.com>
2024-06-03 20:27:43 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 744 ms, Memory: 131996 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -2,43 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	private static int N;
-	private static int M;
-	private static int[][] prefix_sum;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int[][] a = new int[n + 1][n + 1];
+		int[][] prefixSum = new int[n + 1][n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 1; j <= n; j++) {
+				a[i][j] = Integer.parseInt(st.nextToken());
+				prefixSum[i][j] = a[i][j] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
+			}
+		}
+
 		StringBuilder sb = new StringBuilder();
-
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
-		prefix_sum = new int[N + 1][N + 1];
-
-		for (int i = 1; i <= N; i++) {
+		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 1; j <= N; j++) {
-				prefix_sum[i][j] = Integer.parseInt(st.nextToken()) + prefix_sum[i][j - 1];
-			}
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+
+			int result = prefixSum[x2][y2] - prefixSum[x1 - 1][y2] - prefixSum[x2][y1 - 1] + prefixSum[x1 - 1][y1 - 1];
+			sb.append(result).append("\n");
 		}
 
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int startY = Integer.parseInt(st.nextToken());
-			int startX = Integer.parseInt(st.nextToken());
-			int endY = Integer.parseInt(st.nextToken());
-			int endX = Integer.parseInt(st.nextToken());
-
-			int hap = 0;
-			for (int r = startY; r <= endY; r++) {
-				hap += prefix_sum[r][endX] - prefix_sum[r][startX - 1];
-			}
-			sb.append(hap).append("\n");
-		}
-
-		System.out.println(sb.toString());
-
+		System.out.print(sb.toString());
 	}
 }
