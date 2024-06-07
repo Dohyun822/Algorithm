8ac364244ca6fa39edf3254683b700fb9c2ef474
Dohyun822 <yup8229@gmail.com>
2024-06-07 18:26:13 +0900

    [Silver IV] Title: 수들의 합 2, Time: 96 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -2,45 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int N;
-	private static long M;
-	private static int[] arr;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		M = Long.parseLong(st.nextToken());
 
-		arr = new int[N];
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
+		int[] A = new int[N];
 		st = new StringTokenizer(br.readLine(), " ");
-
 		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			A[i] = Integer.parseInt(st.nextToken());
 		}
 
+		int count = 0;
+		int sum = 0;
 		int start = 0;
-		int end = 0;
 
-		int answer = 0;
-		while (end < N) {
-			long hap = 0;
-			for (int i = start; i <= end; i++) {
-				hap += arr[i];
+		for (int end = 0; end < N; end++) {
+			sum += A[end];
+
+			while (sum > M) {
+				sum -= A[start++];
 			}
 
-			if (hap == M) {
-				answer++;
-				end++;
-			} else if (hap < M) {
-				end++;
-			} else {
-				start++;
+			if (sum == M) {
+				count++;
 			}
 		}
 
-		System.out.println(answer);
-
+		System.out.println(count);
 	}
 }
