4af3228ace4c3ee8ba14a5f7f33b2d4bbeebec2c
Dohyun822 <yup8229@gmail.com>
2024-06-29 02:18:33 +0900

    [Gold V] Title: 집합의 표현, Time: 1388 ms, Memory: 56976 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,63 +1,58 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	private static int N;
+	private static int M;
 	private static int[] parent;
-	private static int[] rank;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-		parent = new int[n + 1];
-		rank = new int[n + 1];
-		for (int i = 0; i <= n; i++) {
+		parent = new int[N + 1];
+		for (int i = 0; i <= N; i++) {
 			parent[i] = i;
-			rank[i] = 0;
 		}
 
-		for (int i = 0; i < m; i++) {
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			int operation = Integer.parseInt(st.nextToken());
+
+			int command = Integer.parseInt(st.nextToken());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 
-			if (operation == 0) {
+			if (command == 0) {
 				union(a, b);
-			} else if (operation == 1) {
-				if (find(a) == find(b)) {
-					System.out.println("YES");
-				} else {
+			} else {
+				int rootX = find(a);
+				int rootY = find(b);
+				if (rootX != rootY) {
 					System.out.println("NO");
+				} else {
+					System.out.println("YES");
 				}
 			}
+		}
+
+	}
+
+	private static void union(int a, int b) {
+		int rootX = find(a);
+		int rootY = find(b);
+
+		if (rootX != rootY) {
+			parent[rootY] = rootX;
 		}
 	}
 
 	private static int find(int a) {
-		if (a == parent[a]) {
-			return a;
+		if (parent[a] != a) {
+			parent[a] = find(parent[a]);
 		}
-		return parent[a] = find(parent[a]);
-	}
-
-	private static void union(int a, int b) {
-		int rootA = find(a);
-		int rootB = find(b);
-
-		if (rootA != rootB) {
-			if (rank[rootA] < rank[rootB]) {
-				parent[rootA] = rootB;
-			} else if (rank[rootA] > rank[rootB]) {
-				parent[rootB] = rootA;
-			} else {
-				parent[rootB] = rootA;
-				rank[rootA]++;
-			}
-		}
+		return parent[a];
 	}
 }
