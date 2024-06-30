d828764a312fdc0f8fbf97920734277b295c85f1
Dohyun822 <yup8229@gmail.com>
2024-07-01 02:29:00 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 68 ms, Memory: 11380 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            String name = st.nextToken();
+            int age = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            
+            if (name.equals("#") && age == 0 && weight == 0) {
+                break;
+            }
+            
+            if (age > 17 || weight >= 80) {
+                System.out.println(name + " Senior");
+            } else {
+                System.out.println(name + " Junior");
+            }
+        }
+    }
+}
