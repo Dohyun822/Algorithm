4b07d8a76b13ed3e7fd0ef30abd97fbff2050808
Dohyun822 <yup8229@gmail.com>
2024-07-08 00:49:08 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 1072 ms, Memory: 55656 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -4,73 +4,66 @@ import java.util.*;
 public class Main {
 	private static int N;
 	private static int K;
-	private static int[] dist;
+	private static int[] arr;
+	private static boolean[] visited;
 	private static int[] prev;
+	private static Stack<Integer> stack;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 
-		dist = new int[100001];
+		arr = new int[100001];
+		visited = new boolean[100001];
 		prev = new int[100001];
 
-		Arrays.fill(dist, Integer.MAX_VALUE);
+		arr[N] = 0;
 
-		solve();
+		bfs(N);
 
-		System.out.println(dist[K]);
+		System.out.println(arr[K]);
 
-		path();
-	}
-
-	private static void path() {
-		Stack<Integer> stack = new Stack<>();
-		int cur = K;
-		while (cur != N) {
-			stack.push(cur);
-			cur = prev[cur];
-		}
-		stack.push(N);
+		stack = new Stack<Integer>();
+		getRoute(K);
 
 		while (!stack.isEmpty()) {
 			System.out.print(stack.pop() + " ");
 		}
+
+		System.out.print(K);
 	}
 
-	private static void solve() {
-		Queue<Integer> q = new ArrayDeque<>();
+	private static void getRoute(int k) {
+		int cur = k;
+		if (prev[cur] != -1) {
+			stack.add(prev[cur]);
+			getRoute(prev[cur]);
+		}
+	}
 
-		q.offer(N);
+	private static void bfs(int cur) {
+		Queue<Integer> q = new ArrayDeque<Integer>();
 
-		dist[N] = 0;
+		q.offer(cur);
+		prev[cur] = -1;
+		visited[cur] = true;
 
 		while (!q.isEmpty()) {
-			int cur = q.poll();
-
-			if (cur == K) {
+			int current = q.poll();
+			if (current == K) {
 				return;
 			}
-			int left = cur - 1;
-			int right = cur + 1;
-			int move = cur * 2;
-
-			if (left >= 0 && dist[left] > dist[cur] + 1) {
-				dist[left] = dist[cur] + 1;
-				prev[left] = cur;
-				q.offer(left);
-			}
-			if (right <= 100000 && dist[right] > dist[cur] + 1) {
-				dist[right] = dist[cur] + 1;
-				prev[right] = cur;
-				q.offer(right);
-			}
-			if (move >= 0 && move <= 100000 && dist[move] > dist[cur]) {
-				dist[move] = dist[cur] + 1;
-				prev[move] = cur;
-				q.offer(move);
+			int[] pos = new int[] { current - 1, current + 1, current * 2 };
+			for (int move : pos) {
+				if (move >= 0 && move < 100001 && !visited[move]) {
+					arr[move] = arr[current] + 1;
+					prev[move] = current;
+					visited[move] = true;
+					q.offer(move);
+				}
 			}
 		}
 	}
