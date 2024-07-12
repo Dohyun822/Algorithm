854676ed9a7d20851000a2531029dfa8e43bef09
Dohyun822 <yup8229@gmail.com>
2024-07-12 20:53:56 +0900

    [Gold IV] Title: 공유기 설치, Time: 256 ms, Memory: 29152 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -2,51 +2,58 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int N;
-	private static int C;
-	private static int[] arr;
+    private static int N;
+    private static int C;
+    private static int[] arr;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		N = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-		arr = new int[N];
+        arr = new int[N];
 
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
 
-		Arrays.sort(arr);
+        Arrays.sort(arr);
 
-		int left = 1; // 최소 거리
-		int right = arr[N - 1] - arr[0]; // 최대 거리
-		int result = 0;
+        int left = 1;
+        int right = arr[N - 1] - arr[0];
+        int answer = 0;
 
-		while (left <= right) {
-			int mid = (left + right) / 2; // 중간 거리
-			int lastInstalled = arr[0]; // 첫 번째 집에 공유기 설치
-			int count = 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-			// 공유기 설치
-			for (int i = 1; i < N; i++) {
-				if (arr[i] - lastInstalled >= mid) {
-					lastInstalled = arr[i];
-					count++;
-				}
-			}
+            if (chk(mid)) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
 
-			// 공유기 수 확인 및 거리 조정
-			if (count >= C) {
-				result = mid; // 가능한 거리 갱신
-				left = mid + 1;
-			} else {
-				right = mid - 1;
-			}
-		}
+        }
 
-		System.out.println(result);
-	}
+        System.out.println(answer);
+
+    }
+
+    private static boolean chk(int dis) {
+        int count = 1;
+        int cur = arr[0];
+
+        for (int i = 1; i < N; i++) {
+            if (arr[i] - cur >= dis) {
+                count++;
+                cur = arr[i];
+            }
+            if (count >= C) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
