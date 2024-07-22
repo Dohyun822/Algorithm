5f6eab7d38d04317d31f172cd9c8cb7ae01b7e8f
Dohyun822 <yup8229@gmail.com>
2024-07-22 21:42:43 +0900

    [Gold IV] Title: 부분합, Time: 224 ms, Memory: 23236 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -2,43 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int N;
-	private static int S;
+    private static int N;
+    private static int S;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		N = Integer.parseInt(st.nextToken());
-		S = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
 
-		int[] arr = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+        int[] arr = new int[N];
 
-		int start = 0;
-		int end = 0;
-		int sum = 0;
-		int answer = Integer.MAX_VALUE;
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-		while (true) {
-			if (sum >= S) {
-				answer = Math.min(answer, end - start);
-				sum -= arr[start++];
-			} else if (end == N) {
-				break;
-			} else {
-				sum += arr[end++];
-			}
-		}
+        int start = 0;
+        int end = 0;
+        int hap = 0;
+        int answer = Integer.MAX_VALUE;
+        while (true) {
+            if (hap >= S) {
+                answer = Math.min(answer, end - start);
+                hap -= arr[start++];
+            } else if (end == N) {
+                break;
+            } else {
+                hap += arr[end++];
+            }
+        }
 
-		if (answer == Integer.MAX_VALUE) {
-			System.out.println(0);
-		} else {
-			System.out.println(answer);
-		}
-	}
-
+        if (answer == Integer.MAX_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(answer);
+        }
+    }
 }
