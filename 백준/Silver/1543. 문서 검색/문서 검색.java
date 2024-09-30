6809799592a189411f490c21be2405f5ed24a5bc
Dohyun822 <yup8229@gmail.com>
2024-10-01 03:39:19 +0900

    [Silver V] Title: 문서 검색, Time: 96 ms, Memory: 12964 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String doc = sc.nextLine();
+        String word = sc.nextLine();
+        
+        int i = 0, count = 0;
+        while (i <= doc.length() - word.length()) {
+            if (doc.substring(i, i + word.length()).equals(word)) {
+                count++;
+                i += word.length();
+            } else {
+                i++;
+            }
+        }
+        System.out.println(count);
+    }
+}
